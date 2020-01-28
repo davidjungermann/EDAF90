135e9b74b902360e7321bdf4b7d312377a381ef5
ravedave <dic15dju@student.lu.se>
2020-01-28 17:07:42 +0100

    Lab2 init react

---
+++ Lab2/lab2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <div className="jumbotron text-center">
+         <h1>My Own Salad Bar</h1>
+         <p>Here you can order custom made salads!</p> 
+        </div>
+       <div>
+         Add your own react component here!
+     </div>
+   </div>
+  );
+}
+
+export default App;
