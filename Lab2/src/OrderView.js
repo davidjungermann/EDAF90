bcb3a5e1a3578361c30baacfdb4a61595d3ea095
ravedave <dic15dju@student.lu.se>
2020-02-02 23:32:08 +0100

    lab2 finished

--- Lab2/src/OrderView.js
+++ Lab2/src/OrderView.js
@@ -1,20 +1,24 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css'
+import "./OrderView.css"
 import React from "react";
+
 const shortid = require('shortid');
+
 
 class OrderView extends React.Component {
 
     render() {
         return (
-            <ul className="container w-50" id="salads">
+            <ul className="container">
                 {this.props.orderList.map(salad =>
-                    <li key={shortid.generate()} className='list-group-item'>
+                    <li key={shortid.generate()} className='list-group-item fixed'>
                         {salad.print(salad)}
-                        <button type='button' className="btn btn-danger" onClick={() => this.props.saladRemove(salad)}>Ta bort</button>
-                        <span class="badge badge-primary badge-pill">{salad.price()}</span>
-                    </li>)}
+                        <button type='button' className="btn btn-danger right-button" onClick={() => this.props.saladRemove(salad)}>Ta bort</button>
+                        <span className="badge badge-primary badge-pill pill" >{salad.price()}</span>
+                    </li>)
+                }
             </ul>
 
         );
