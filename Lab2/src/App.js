1c162992e0a40cd5f4933a4ef65e6e3a01a7d0ff
ravedave <dic15dju@student.lu.se>
2020-02-02 15:23:13 +0100

    saving salad state in app

--- Lab2/src/App.js
+++ Lab2/src/App.js
@@ -3,25 +3,40 @@ import './App.css';
 import inventory from './inventory.ES6';
 import ComposeSaladModal from "./ComposeSaladModal";
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salads: []
+    };
+  }
+
+  saladSubmit(salad) {
+    let tempSalads = [...this.state.salads];
+    tempSalads.push(salad)
+    this.setState({ salads: tempSalads })
+  }
+
+  render() {
+    return (
       <div>
-          <div className="jumbotron text-center">
-              <h1 className="display-4">Brunt & Bajs</h1>
-              <p className="lead">Lunds äckligaste sallad</p>
-              <hr className="my-4" />
-              <p>Gör din rövsallad här!</p>
-              <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-toggle="modal"
-                  data-target="#ComposeSaladModal"
-              >
-                  Komponera din egen sallad
+        <div className="jumbotron text-center">
+          <h1 className="display-4">Brunt & Bajs</h1>
+          <p className="lead">Lunds äckligaste sallad</p>
+          <hr className="my-4" />
+          <p>Gör din rövsallad här!</p>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-toggle="modal"
+            data-target="#ComposeSaladModal"
+          >
+            Komponera din egen sallad
       </button>
-          </div>
-          <ComposeSaladModal inventory={inventory} />
+        </div>
+        <ComposeSaladModal inventory={inventory} />
       </div>
-  );
+    );
+  }
 }
 export default App;
