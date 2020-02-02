73eef08903d403291659fd3bbed4c4a475e1c8dd
ravedave <dic15dju@student.lu.se>
2020-02-02 23:32:27 +0100

    lab2 finished

--- Lab2/src/OrderView.js
+++ Lab2/src/OrderView.js
@@ -1,7 +1,6 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css'
-import "./OrderView.css"
 import React from "react";
 
 const shortid = require('shortid');
